7ee7f7e91e35e024fddbe1603d38ba19819fc07b
Dayoung Lee <dayoung31668@gmail.com>
2019-07-24 13:40:24 -0400

    gets products from marketplace

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,73 +4,57 @@ class Store extends Component {
   constructor() {
     super();
     this.state = {
-      data: [
-        {
-          name: 'Water Bottle',
-          picture:
-            'https://images-na.ssl-images-amazon.com/images/I/61dtC2URIPL._SX425_.jpg',
-          price: 10,
-        },
-        {
-          name: 'Adidas Stan Smith',
-          picture:
-            'https://shop.r10s.jp/auc-rodeo/cabinet/adidas/m20325-11.jpg',
-          price: 20,
-        },
-        {
-          name: 'School Supplies',
-          picture:
-            'https://www.mesaunitedway.org/helenshope/wp-content/uploads/2017/08/School-Supplies.jpg',
-          price: 10,
-        },
-        {
-          name: 'Coffee',
-          picture:
-            'https://milklife.com/sites/default/files/styles/recipe_zoomable_images_large/public/main_image/Recipe/2013/09/18/Protein-Packed%20Latte_square.jpg?itok=vwVtOEyA',
-          price: 5,
-        },
-      ],
+      store: [],
+      featured: {},
+      rewards: [],
     };
   }
 
   componentDidMount() {
     const { firebase } = this.props;
-    console.log(firebase.exportToDB().collection("marketplace").get().then((querySnapshot) => {
-      const data = querySnapshot.docs.map(doc => doc.data());
-      console.log(data);
-    }));
+
+    firebase
+      .exportToDB()
+      .collection('marketplace')
+      .get()
+      .then(querySnapshot => {
+        const data = querySnapshot.docs.map(doc => doc.data());
+        this.setState({
+          store: data.sort((a, b) =>
+            a.berries < b.berries ? 1 : b.berries < a.berries ? -1 : 0
+          ),
+        });
+        this.setState({ featured: this.state.store[0] });
+        this.setState({ rewards: this.state.store.slice(1) });
+      });
   }
+
   render() {
-    let featured = null;
-    let rewards = [];
-    this.state.data.sort((a, b) =>
-      a.price < b.price ? 1 : b.price < a.price ? -1 : 0
-    );
-    featured = this.state.data[0];
-    rewards = this.state.data.slice(1);
-    
+    let { featured, rewards } = this.state;
+    if (!featured) featured = {};
+
     return (
       <div id="store">
         <div id="storeBody">
           <div id="featured">
-            <img id="featuredImg" src={featured.picture} alt="" />
+            <img id="featuredImg" src={featured.image} alt="" />
             <div id="featuredInfo">
               <div className="rewardsTxt">
                 <h1 id="featuredName">{featured.name}</h1>
                 <img src="/images/redBerryIcon.png" alt="" />
-                {featured.price}
+                {featured.berries}
               </div>
             </div>
           </div>
           <div id="rewards">
             {rewards.map(reward => (
               <div id="rewardCard">
-                <img src={reward.picture} alt="" />
+                <img src={reward.image} alt="" />
                 <div id="rewardsInfo">
                   <div className="rewardsTxt">
                     <h1>{reward.name}</h1>
                     <img src="/images/redBerryIcon.png" alt="" />
-                    {reward.price}
+                    {reward.berries}
                   </div>
                 </div>
               </div>
